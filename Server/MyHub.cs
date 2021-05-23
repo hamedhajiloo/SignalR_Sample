37717eb4f519404b62e2e7c0fefdb8529b092022
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-05-23 11:32:17 +0430

    Add project files.

---
+++ Server/MyHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class MyHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("Test", message);
+        }
+    }
+}
