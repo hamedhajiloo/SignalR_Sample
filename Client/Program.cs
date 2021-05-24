2cd1ae5c28c5794f05e49e22ada3be2a5bb9d433
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-05-24 18:49:07 +0430

    Update Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,15 +8,17 @@ namespace Client
     {
         static async Task Main(string[] args)
         {
+            var connection = await ConnectToHubAsync();
             while (true)
             {
                 Console.Write("Insert message: ");
                 var text = Console.ReadLine();
-                await ConnectToHubAsync(text);
+                await connection.InvokeAsync("SendMessage", text);
+
             }
         }
 
-        static async Task ConnectToHubAsync(string text)
+        static async Task<HubConnection> ConnectToHubAsync()
         {
             bool started = false;
             HubConnection connection = new HubConnectionBuilder()
@@ -26,12 +28,12 @@ namespace Client
             connection.Closed += async (error) =>
             {
                 await Task.Delay(TimeSpan.FromSeconds(5));
-                await StartAsync(connection, started, text);
+                await StartAsync(connection, started);
             };
 
-            await StartAsync(connection, started, text);
-
-            static async Task StartAsync(HubConnection connection, bool started, string text)
+            await StartAsync(connection, started);
+            return connection;
+            static async Task StartAsync(HubConnection connection, bool started)
             {
                 if (started)
                     return;
@@ -42,13 +44,12 @@ namespace Client
                     {
                         await connection.StartAsync();
                         started = true;
-                        await connection.InvokeAsync("SendMessage", text);
 
                     }
                     catch
                     {
                         await Task.Delay(TimeSpan.FromMinutes(1));
-                        await StartAsync(connection, started, text);
+                        await StartAsync(connection, started);
                     }
                 }
             }
